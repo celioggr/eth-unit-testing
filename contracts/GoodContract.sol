8c04416c202f13abe54b3e60be5bfa622b7ad782
celioggr <celioggr@gmail.com>
2020-05-13 10:45:46 +0100

    Added files to use in brownie test

---
+++ contracts/GoodContract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+import "./ERC20.sol";
+import "./ERC20Detailed.sol";
+
+contract GoodContract is ERC20, ERC20Detailed{
+
+  constructor(uint256 initialSupply, string memory _name, string memory _symbol, uint8 _decimals)
+  ERC20Detailed(_name,_symbol,_decimals) public  {
+    _mint(msg.sender, initialSupply);
+  }
+
+}
